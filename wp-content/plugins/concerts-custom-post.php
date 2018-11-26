d9754424679ab3ac34e10c14ec088d069692377e
Student <student@CVS-R212D010.nic.bc.ca>
2018-11-26 10:10:33 -0800

    adds concerts custom posts plugin

---
+++ wp-content/plugins/concerts-custom-post.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name:  Concerts Custom Post
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Basic WordPress Plugin Header Comment
+Version:      20160911
+Author:       WordPress.org
+Author URI:   https://developer.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
+
+/*
+Concerts Custom Post is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Concerts Custom Post is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Concerts Custom Post. If not, see {URI to Plugin License}.
+*/
